b13ce64911f131909138b446b52ee95affb88cb1
erichengyc <68725887+erichengyc@users.noreply.github.com>
2021-10-11 18:47:09 +0930

    import the map
    --npm install react-native-maps --save-exact
    
    source: https://github.com/react-native-maps/react-native-maps

--- screens/Map.js
+++ screens/Map.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
 import { Text, StyleSheet, View } from "react-native";
+import MapView from "react-native-maps";
 
 export default class Map extends Component {
+  renderHeader() {
+    return <View style={styles.header}>
+        <Text>Header</Text>
+    </View>;
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Map</Text>
+          {this.renderHeader()}
+        <MapView
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          style={styles.map}
+        />
       </View>
     );
   }
@@ -14,8 +30,17 @@ export default class Map extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  header: {
+    flex: 1,
+    height: 100,
+  },
+  map: {
+    flex: 1,
+    // width: 100,
+    // height: 100,
   },
 });
