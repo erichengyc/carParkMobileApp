0c5f064e7dfa66dffd33939d0dab1043252e2ca4
erichengyc <68725887+erichengyc@users.noreply.github.com>
2021-10-11 15:19:28 +0930

    initial

---
+++ screens/Map.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, Stylesheet, View } from "react-native";
+
+export default class Map extends Component{
+    render() {
+        return (
+            <View>
+                <Text>textInComponent</Text>
+            </View>
+        )
+    }
+}
+
+const styles = Stylesheet.create()
