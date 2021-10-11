607618f3bbaf156f87d4b8bb2bc9d6e5fe772b3b
erichengyc <68725887+erichengyc@users.noreply.github.com>
2021-10-11 15:35:00 +0930

    import map.js to app.js

--- screens/Map.js
+++ screens/Map.js
@@ -1,14 +1,21 @@
-import React from "react";
-import { Text, Stylesheet, View } from "react-native";
+import React, { Component } from "react";
+import { Text, StyleSheet, View } from "react-native";
 
-export default class Map extends Component{
-    render() {
-        return (
-            <View>
-                <Text>textInComponent</Text>
-            </View>
-        )
-    }
+export default class Map extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Map</Text>
+      </View>
+    );
+  }
 }
 
-const styles = Stylesheet.create()
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
