98cd9efc2aca33716aa5b1e2d410dc9f0ba0a0f6
erichengyc <68725887+erichengyc@users.noreply.github.com>
2021-10-11 23:49:07 +0930

    added the marker

--- screens/Map.js
+++ screens/Map.js
@@ -7,12 +7,13 @@ import {
   TouchableOpacity,
   Picker,
   FlatList,
+  TouchableWithoutFeedback,
 } from "react-native";
 import MapView from "react-native-maps";
 import { Ionicons } from "@expo/vector-icons";
 
+const { Marker } = MapView;
 const { height, width } = Dimensions.get("screen");
-
 const parkings = [
   {
     id: 1,
@@ -21,9 +22,9 @@ const parkings = [
     rating: 4.2,
     spots: 20,
     free: 10,
-    location: {
-      lat: 37.78845,
-      lng: -122.4344,
+    coordinate: {
+      latitude: 37.78735,
+      longitude: -122.4344,
     },
   },
   {
@@ -33,9 +34,9 @@ const parkings = [
     rating: 3.8,
     spots: 25,
     free: 20,
-    location: {
-      lat: 37.78815,
-      lng: -122.4314,
+    coordinate: {
+      latitude: 37.78845,
+      longitude: -122.4314,
     },
   },
   {
@@ -45,9 +46,9 @@ const parkings = [
     rating: 4.9,
     spots: 50,
     free: 25,
-    location: {
-      lat: 37.78835,
-      lng: -122.4334,
+    coordinate: {
+      latitude: 37.78615,
+      longitude: -122.4334,
     },
   },
 ];
@@ -55,6 +56,7 @@ const parkings = [
 export default class Map extends Component {
   state = {
     hours: {},
+    active: null,
   };
 
   componentDidMount() {
@@ -63,7 +65,7 @@ export default class Map extends Component {
     parkings.map((parking) => {
       hours[parking.id] = 1;
     });
-    this.setState({ hours })
+    this.setState({ hours });
   }
 
   renderHeader() {
@@ -78,12 +80,16 @@ export default class Map extends Component {
     const { hours } = this.state;
 
     return (
-      <View key={"parking-${item.id}"} style={styles.parking}>
-        <View style={{ flex: 1, flexDirection: "column" }}>
-          <Text style={{ fontSize: 16 }}>
-            x {item.spots} {item.title}
-          </Text>
-          {/* <Picker
+      <TouchableWithoutFeedback>
+        <View
+          key={"parking-${item.id}"}
+          style={[styles.parking, styles.shadow]}
+        >
+          <View style={{ flex: 1, flexDirection: "column" }}>
+            <Text style={{ fontSize: 16 }}>
+              x {item.spots} {item.title}
+            </Text>
+            {/* <Picker
             selectedValue={this.state.hours[item.id || 1 ]}
             style={{ height: 50, width: 100 }}
             itemStyle={{ color: 'red'}}
@@ -102,93 +108,89 @@ export default class Map extends Component {
             <Picker.Item label="07:00" value={7} />
           </Picker> */}
 
-          <View
-            style={{
-              width: 100,
-              borderRadius: 6,
-              borderColor: "grey",
-              borderWidth: 0.5,
-              padding: 4,
-            }}
-          >
-            <Text style={{ fontSize: 16 }}>05:00</Text>
-          </View>
-        </View>
-        <View style={{ flex: 1.5, flexDirection: "row" }}>
-          <View
-            style={{
-              flex: 0.5,
-              justifyContent: "center",
-              marginHorizontal: 12,
-            }}
-          >
             <View
               style={{
-                flex: 1,
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
+                width: 100,
+                borderRadius: 6,
+                borderColor: "grey",
+                borderWidth: 0.5,
+                padding: 4,
               }}
             >
-              <Ionicons name="ios-pricetag" size={16} color="#7D818A" />
-              <Text> ${item.price} </Text>
-            </View>
-            <View
-              style={{
-                flex: 1,
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
-              <Ionicons name="ios-star" size={16} color="#7D818A" />
-              <Text> {item.rating} </Text>
+              <Text style={{ fontSize: 16 }}>05:00</Text>
             </View>
           </View>
-          <TouchableOpacity style={styles.buy}>
-            <View style={{ flex: 1, justifyContent: "center" }}>
-              <Text style={{ fontSize: 25, color: "white" }}>
-                ${item.price * 2}
-              </Text>
-              <Text style={{ color: "white" }}>
-                {item.price}x{hours[item.id]} hrs
-              </Text>
-            </View>
+          <View style={{ flex: 1.5, flexDirection: "row" }}>
             <View
               style={{
                 flex: 0.5,
                 justifyContent: "center",
-                alignItems: "center",
+                marginHorizontal: 12,
               }}
             >
-              <Text style={{ color: "white" }}>&gt;</Text>
+              <View
+                style={{
+                  flex: 1,
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <Ionicons name="ios-pricetag" size={16} color="#7D818A" />
+                <Text> ${item.price} </Text>
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <Ionicons name="ios-star" size={16} color="#7D818A" />
+                <Text> {item.rating} </Text>
+              </View>
             </View>
-          </TouchableOpacity>
+            <TouchableOpacity style={styles.buy}>
+              <View style={{ flex: 1, justifyContent: "center" }}>
+                <Text style={{ fontSize: 25, color: "white" }}>
+                  ${item.price * 2}
+                </Text>
+                <Text style={{ color: "white" }}>
+                  {item.price}x{hours[item.id]} hrs
+                </Text>
+              </View>
+              <View
+                style={{
+                  flex: 0.5,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Text style={{ color: "white" }}>&gt;</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
+      </TouchableWithoutFeedback>
     );
   }
 
   renderParkings() {
     return (
-        <FlatList
-            horizontal
-            pagingEnabled
-            scrollEnabled
-            showsHorizontalScrollIndicator={false}
-            scrollEventThrottle={16}
-            snapToAlignment="center"
-            onScroll={(props) => console.log("onScroll", props)}
-            style={styles.parkings}
-            data={parkings}
-            keyExtractor={(item, index) => '$(item.id)'}
-            renderItem={({ item }) => this.renderParking(item) }
-        />
-
-
-    //   <ScrollView>
-    //     {parkings.map((parking) => this.renderParking(parking))}
-    //   </ScrollView>
+      <FlatList
+        horizontal
+        pagingEnabled
+        scrollEnabled
+        showsHorizontalScrollIndicator={false}
+        scrollEventThrottle={16}
+        snapToAlignment="center"
+        onScroll={(props) => console.log("onScroll", props)}
+        style={styles.parkings}
+        data={parkings}
+        keyExtractor={(item, index) => "$(item.id)"}
+        renderItem={({ item }) => this.renderParking(item)}
+      />
     );
   }
 
@@ -200,11 +202,25 @@ export default class Map extends Component {
           initialRegion={{
             latitude: 37.78825,
             longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
+            latitudeDelta: 0.0122,
+            longitudeDelta: 0.0121,
           }}
           style={styles.map}
-        />
+        >
+          {parkings.map((parking) => (
+            <Marker key={"marker-${parking.id"} coordinate={parking.coordinate}>
+              <View style={[styles.marker, styles.shadow]}>
+                <Text style={{ color: "#840815", fontWeight: "bold" }}>
+                  ${parking.price}{" "}
+                </Text>
+                <Text style={{ color: "#7D818A" }}>
+                  ({parking.free}/{parking.spots})
+                </Text>
+              </View>
+            </Marker>
+          ))}
+        </MapView>
+
         {this.renderParkings()}
       </View>
     );
@@ -220,6 +236,7 @@ const styles = StyleSheet.create({
     flex: 0.5,
     // alignItems: "center",
     justifyContent: "center",
+    paddingHorizontal: 24,
   },
   map: {
     flex: 3,
@@ -228,7 +245,8 @@ const styles = StyleSheet.create({
     position: "absolute",
     right: 0,
     left: 0,
-    bottom: 24,
+    bottom: 0,
+    paddingBottom: 24,
   },
   parking: {
     flexDirection: "row",
@@ -244,5 +262,22 @@ const styles = StyleSheet.create({
     padding: 12,
     backgroundColor: "#D25260",
     borderRadius: 6,
+  },
+  marker: {
+    flexDirection: "row",
+    backgroundColor: "white",
+    borderRadius: 24,
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+  },
+  shadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 24,
   },
 });
